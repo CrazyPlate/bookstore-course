093e06f6616b635caabb5761acb3e0e1896eaa70
Tomek Telesz <tomektelesz@gmail.com>
2018-11-27 17:54:33 +0100

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AdminPanel from './AdminPanel';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+
+class App extends React.Component {
+
+   render() {
+      return (
+         <div>
+            <Header />
+            <Order />
+            <Inventory />
+            <AdminPanel />
+         </div>
+      )
+   }
+
+}
+
+export default App;
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Order extends React.Component {
+   render() {
+      return <div>Zamówienie</div>
+   }
+}
+
+export default Order;
